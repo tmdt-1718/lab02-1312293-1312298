74a06cd16658e02db84fa9a3669ac53b0b910af0
Lê Thanh Khương <31948875+1312298@users.noreply.github.com>
2017-10-25 12:43:51 +0700

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/index', to: 'lab02_ecommerce#index'
+  get 'index', to: 'lab02_ecommerce#index'
   get 'login', to: 'lab02_ecommerce#index'
   post 'login', to: 'lab02_ecommerce#login'
-  post '/index', to: 'lab02_ecommerce#create'
+  post 'index', to: 'lab02_ecommerce#create'
   get '', to: 'lab02_ecommerce#index'
-  get '/register', to: 'lab02_ecommerce#register'
-  get '/messenger', to:'messenger#index'
-  get '/inbox', to:'inbox#index'
+  get 'register', to: 'lab02_ecommerce#register'
+  get 'messenger', to:'messenger#index'
+  get 'inbox', to:'inbox#index'
 
-  post '/addfriend', to: 'messenger#addfriend'
-  post '/sendMessage', to: 'inbox#sendMessage'
-  delete '/sessions/logout', to: 'sessions#destroy', as: :logout
+  post 'addfriend', to: 'messenger#addfriend'
+  post 'sendMessage', to: 'inbox#sendMessage'
+  delete 'sessions/logout', to: 'sessions#destroy', as: :logout
 end
